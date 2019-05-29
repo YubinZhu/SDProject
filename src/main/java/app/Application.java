aafae2674f122b1fe29434de514de12325ce37ed
yubzhu <651445686@qq.com>
2019-05-29 23:14:35 +0800

    version 0.1: initial project.

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,15 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yubzhu on 19-4-30
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
