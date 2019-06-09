9e0d8221d7c41f23e34c83ca6f0e0251fe475b83
yubzhu <651445686@qq.com>
2019-06-09 16:05:23 +0800

    :art: update

--- src/main/java/app/controller/CoordinatesController.java
+++ src/main/java/app/controller/CoordinatesController.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
 import javax.servlet.http.HttpServletRequest;
 import java.sql.ResultSet;
 import java.sql.SQLException;
